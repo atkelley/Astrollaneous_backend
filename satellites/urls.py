f46c18b7e4244775ad36d4f563ec4158c2d4bdc0
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-02-16 14:36:46 -0500

    Issue with Satellites API url

--- satellites/urls.py
+++ satellites/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('', views.satellites),
-  path('<str:name>', views.satellite),
+  path('space', views.satellites),
+  path('space/<str:name>', views.satellite),
 ]
