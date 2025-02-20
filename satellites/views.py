50c42e9535689dac6d8125d6cea5f50783e4f9f8
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-02-20 14:40:22 -0500

    Redid server-side call to Celestrak in getSatellite view

--- satellites/views.py
+++ satellites/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, FileResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import SatelliteSerializer
 from .models import Satellite
-# import requests, tle2czml, os
-
+import requests, tle2czml, os
+from pathlib import Path
 
 @api_view(['GET'])
 def satellites(request):
@@ -17,33 +17,48 @@ def satellites(request):
       for value in value_list:
         group_by_value[value] = Satellite.objects.values().filter(type=value)
       
-      return Response(group_by_value)
+      reversed = dict(sorted(group_by_value.items(), reverse=True))
+      return Response(reversed)
     except Satellite.DoesNotExist: 
-      return JsonResponse({'error': 'Satellites not found.'}, status=status.HTTP_404_NOT_FOUND)
+      return JsonResponse({'error': 'Satellites not found.'}, status=404)
 
 
 @api_view(['GET'])
 def satellite(request, name):
   if request.method == "GET":
-    satellites = list(Satellite.objects.values())
-    satellites_list = [entry for entry in satellites]
+    tle_filename = "{}.txt".format(name)
+    czml_filename = "{}.czml".format(name)
 
-    result = None
-    for satellite in satellites_list:
-      for key, value in satellite.items():
-        if key == 'name' and value == name:
-          result = satellite
-          break
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    tle2czml_path = os.path.join(BASE_DIR, 'staticfiles', 'tle2czml')
 
-    # base_url = "https://celestrak.org/NORAD/elements/gp.php?GROUP={}&FORMAT=tle".format(name)
-    # r = requests.get(base_url)
+    if not os.path.exists(tle2czml_path):
+      os.makedirs(tle2czml_path)
 
-    # input_file = "static/mySpaceStuff/tle2czml/tle_{}.txt".format(name)
-    
-    # with open(input_file, 'wb') as f:
-    #   f.write(r.content)
-    # f.close()
+    for folder in ['tle', 'czml']:
+      if not os.path.exists(os.path.join(tle2czml_path, folder)):
+        os.makedirs(os.path.join(tle2czml_path, folder))
 
-    # output_file = "static/mySpaceStuff/tle2czml/tle_{}.czml".format(name)
-    # tle2czml.create_czml(input_file, outputfile_path=output_file)
-    return JsonResponse(result, safe=False)
+    tle_file_path = os.path.join(tle2czml_path, "tle", tle_filename)
+    czml_file_path = os.path.join(tle2czml_path, "czml", czml_filename)
+
+    if not os.path.exists(czml_file_path):
+      if not os.path.exists(tle_file_path):
+        response = requests.get("https://celestrak.org/NORAD/elements/gp.php?GROUP={}&FORMAT=tle".format(name))
+      
+        try:
+          with open(tle_file_path, 'wb') as file:
+            file.write(response.content)
+          file.close()
+        except Exception as e:
+          print(f"Error occurred: {e}")
+          return HttpResponse("An error occurred writing the TLE file", status=400)
+
+      tle2czml.create_czml(tle_file_path, outputfile_path=czml_file_path)
+
+    try:
+      file = open(czml_file_path, 'rb')
+      return FileResponse(file, as_attachment=True, filename=czml_filename)
+
+    except FileNotFoundError:
+      return Response({"error": ".czml file not found"}, status=404)
